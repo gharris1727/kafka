4074d38b26b97e102fe8bf0c24ffaa96ff0947a1
Greg Harris <greg.harris@aiven.io>
2023-01-25 18:06:19 -0800

    fixup: test failures with poorly packaged plugins
    Signed-off-by: Greg Harris <greg.harris@aiven.io>

--- connect/runtime/src/test/java/org/apache/kafka/connect/runtime/isolation/TestPlugins.java
+++ connect/runtime/src/test/java/org/apache/kafka/connect/runtime/isolation/TestPlugins.java
@@ -124,7 +124,7 @@ public class TestPlugins {
         /**
          * A plugin which is incorrectly packaged, and is missing a superclass definition.
          */
-        FAIL_TO_INITIALIZE_MISSING_SUPERCLASS("fail-to-initialize", "test.plugins.MissingSuperclass", true, REMOVE_CLASS_FILTER),
+        FAIL_TO_INITIALIZE_MISSING_SUPERCLASS("fail-to-initialize", "test.plugins.MissingSuperclass", false, REMOVE_CLASS_FILTER),
         /**
          * A plugin which is packaged with other incorrectly packaged plugins, but itself has no issues loading.
          */
@@ -132,31 +132,31 @@ public class TestPlugins {
         /**
          * A connector which is incorrectly packaged, and throws during static initialization.
          */
-        FAIL_TO_INITIALIZE_STATIC_INITIALIZER_THROWS_CONNECTOR("fail-to-initialize", "test.plugins.StaticInitializerThrowsConnector", true, REMOVE_CLASS_FILTER),
+        FAIL_TO_INITIALIZE_STATIC_INITIALIZER_THROWS_CONNECTOR("fail-to-initialize", "test.plugins.StaticInitializerThrowsConnector", false, REMOVE_CLASS_FILTER),
         /**
          * A plugin which is incorrectly packaged, which throws an exception from the {@link Versioned#version()} method.
          */
-        FAIL_TO_INITIALIZE_VERSION_METHOD_THROWS_CONNECTOR("fail-to-initialize", "test.plugins.VersionMethodThrowsConnector", true, REMOVE_CLASS_FILTER),
+        FAIL_TO_INITIALIZE_VERSION_METHOD_THROWS_CONNECTOR("fail-to-initialize", "test.plugins.VersionMethodThrowsConnector", false, REMOVE_CLASS_FILTER),
         /**
          * A plugin which is incorrectly packaged, which throws an exception from default constructor.
          */
-        FAIL_TO_INITIALIZE_DEFAULT_CONSTRUCTOR_THROWS_CONNECTOR("fail-to-initialize", "test.plugins.DefaultConstructorThrowsConnector", true, REMOVE_CLASS_FILTER),
+        FAIL_TO_INITIALIZE_DEFAULT_CONSTRUCTOR_THROWS_CONNECTOR("fail-to-initialize", "test.plugins.DefaultConstructorThrowsConnector", false, REMOVE_CLASS_FILTER),
         /**
          * A plugin which is incorrectly packaged, which has a private default constructor.
          */
-        FAIL_TO_INITIALIZE_DEFAULT_CONSTRUCTOR_PRIVATE_CONNECTOR("fail-to-initialize", "test.plugins.DefaultConstructorPrivateConnector", true, REMOVE_CLASS_FILTER),
+        FAIL_TO_INITIALIZE_DEFAULT_CONSTRUCTOR_PRIVATE_CONNECTOR("fail-to-initialize", "test.plugins.DefaultConstructorPrivateConnector", false, REMOVE_CLASS_FILTER),
         /**
          * A plugin which is incorrectly packaged, which has a private default constructor.
          */
-        FAIL_TO_INITIALIZE_NO_DEFAULT_CONSTRUCTOR_CONNECTOR("fail-to-initialize", "test.plugins.NoDefaultConstructorConnector", true, REMOVE_CLASS_FILTER),
+        FAIL_TO_INITIALIZE_NO_DEFAULT_CONSTRUCTOR_CONNECTOR("fail-to-initialize", "test.plugins.NoDefaultConstructorConnector", false, REMOVE_CLASS_FILTER),
         /**
          * A plugin which is incorrectly packaged, which throws an exception from the {@link Versioned#version()} method.
          */
-        FAIL_TO_INITIALIZE_INNER_CLASS_CONNECTOR("fail-to-initialize", "test.plugins.OuterClass$InnerClass", true, REMOVE_CLASS_FILTER),
+        FAIL_TO_INITIALIZE_INNER_CLASS_CONNECTOR("fail-to-initialize", "test.plugins.OuterClass$InnerClass", false, REMOVE_CLASS_FILTER),
         /**
          * A plugin which is incorrectly packaged, which throws an exception from the {@link Versioned#version()} method.
          */
-        FAIL_TO_INITIALIZE_STATIC_INITIALIZER_THROWS_REST_EXTENSION("fail-to-initialize", "test.plugins.StaticInitializerThrowsRestExtension", true, REMOVE_CLASS_FILTER);
+        FAIL_TO_INITIALIZE_STATIC_INITIALIZER_THROWS_REST_EXTENSION("fail-to-initialize", "test.plugins.StaticInitializerThrowsRestExtension", false, REMOVE_CLASS_FILTER);
 
         private final String resourceDir;
         private final String className;
