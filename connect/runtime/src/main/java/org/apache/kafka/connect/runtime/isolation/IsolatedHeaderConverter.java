d96f6994e5a1c58b39b6abdc4a9cdb69e2bb2630
Greg Harris <greg.harris@aiven.io>
2023-01-26 17:32:01 -0800

    Add IsolatedHeaderConverter
    Signed-off-by: Greg Harris <greg.harris@aiven.io>

---
+++ connect/runtime/src/main/java/org/apache/kafka/connect/runtime/isolation/IsolatedHeaderConverter.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kafka.connect.runtime.isolation;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaAndValue;
+import org.apache.kafka.connect.storage.HeaderConverter;
+
+import java.util.Map;
+
+public class IsolatedHeaderConverter extends IsolatedPlugin<HeaderConverter> implements AutoCloseable {
+
+    IsolatedHeaderConverter(Plugins plugins, HeaderConverter delegate) {
+        super(plugins, delegate, PluginType.HEADER_CONVERTER);
+    }
+
+    public SchemaAndValue toConnectHeader(String topic, String headerKey, byte[] value) throws Exception {
+        return isolate(() -> delegate.toConnectHeader(topic, headerKey, value));
+    }
+
+    public byte[] fromConnectHeader(String topic, String headerKey, Schema schema, Object value) throws Exception {
+        return isolate(() -> delegate.fromConnectHeader(topic, headerKey, schema, value));
+    }
+
+    public ConfigDef config() throws Exception {
+        return isolate(delegate::config);
+    }
+
+    public void close() throws Exception {
+        isolateV(delegate::close);
+    }
+
+    public void configure(Map<String, ?> configs) throws Exception {
+        isolateV(delegate::configure, configs);
+    }
+}
